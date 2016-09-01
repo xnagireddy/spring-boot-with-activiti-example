b811ec376b83c9225cbea1d9e0a4217e0b9a0bbe
X Nagireddy <xnagireddy@gmail.com>
2016-09-01 13:35:06 +0530

    Updated to use gradle build process.

--- src/test/java/org/activiti/HireProcessTest.java
+++ src/test/java/org/activiti/HireProcessTest.java
@@ -1,5 +1,9 @@
 package org.activiti;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.activiti.engine.HistoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
@@ -11,20 +15,16 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.subethamail.wiser.Wiser;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = {MyApp.class})
+@RunWith(SpringRunner.class)
+@SpringBootTest
+//@SpringApplicationConfiguration(classes = {MyApp.class})
 @WebAppConfiguration
-@IntegrationTest
+//@IntegrationTest
 public class HireProcessTest {
 
     @Autowired
